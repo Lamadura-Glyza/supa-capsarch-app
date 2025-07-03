48837d96b31660a7222d328643e4697c39f5d438
Lamadura-Glyza <lamaduramgj@gmail.com>
2025-07-03 19:56:52 +0800

    First Commit

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import SignupScreen from '../components/SignupScreen';
 import SplashScreen from '../components/SplashScreen';
 
 export default function App() {
